535209f150d98f270a4b2bd6172d45f5f0bed074
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-03-06 17:05:10 +0700

    [REFACTOR]separate PaymentData interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.PaymentData;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -45,7 +46,7 @@ public class Payment {
             throw new IllegalArgumentException();
         }
 
-        if (!PaymentData.checkData(method, paymentData)){
+        if (!(PaymentData.checkData(method, paymentData))){
             throw new IllegalArgumentException();
         } else {
             this.paymentData = paymentData;
@@ -60,44 +61,5 @@ public class Payment {
             throw new IllegalArgumentException();
         }
     }
-
-}
-
-
-
-interface PaymentData {
-
-
-    public static boolean checkData(String method, HashMap<String, String> param){
-        Set<String> paramKeySet = param.keySet();
-
-        return switch (method) {
-            case "VOUCHER_CODE" -> {
-                Set<String> voucherCodeKeySet = getVoucherCodeKeySet();
-                yield paramKeySet.equals(voucherCodeKeySet);
-            }
-            case "CASH_ON_DELIVERY" -> {
-                Set<String> cashOnDeliveryKeySet = getCashOnDeliveryKeySet();
-                yield paramKeySet.equals(cashOnDeliveryKeySet);
-            }
-            default -> false;
-        };
-    }
-
-    public static Set<String> getVoucherCodeKeySet(){
-        Set<String> voucherCodeKeys = new HashSet<>();
-        voucherCodeKeys.add("voucherCode");
-
-        return voucherCodeKeys;
-    }
-
-    public static Set<String> getCashOnDeliveryKeySet(){
-        Set<String> cashOnDeliveryKeys = new HashSet<>();
-        cashOnDeliveryKeys.add("address");
-        cashOnDeliveryKeys.add("deliveryFee");
-
-        return cashOnDeliveryKeys;
-    }
-
 
 }
---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentData.java
@@ -0,0 +1,52 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public interface PaymentData {
+    public static boolean checkData(String method, HashMap<String, String> param){
+        Set<String> paramKeySet = param.keySet();
+
+        return switch (method) {
+            case "VOUCHER_CODE" -> {
+                Set<String> voucherCodeKeySet = getVoucherCodeKeySet();
+                yield paramKeySet.equals(voucherCodeKeySet);
+            }
+            case "CASH_ON_DELIVERY" -> {
+                Set<String> cashOnDeliveryKeySet = getCashOnDeliveryKeySet();
+                yield paramKeySet.equals(cashOnDeliveryKeySet);
+            }
+            default -> false;
+        };
+    }
+
+    public static Set<String> getVoucherCodeKeySet(){
+        Set<String> voucherCodeKeys = new HashSet<>();
+        voucherCodeKeys.add("voucherCode");
+
+        return voucherCodeKeys;
+    }
+
+    public static Set<String> getCashOnDeliveryKeySet(){
+        Set<String> cashOnDeliveryKeys = new HashSet<>();
+        cashOnDeliveryKeys.add("address");
+        cashOnDeliveryKeys.add("deliveryFee");
+
+        return cashOnDeliveryKeys;
+    }
+
+    public static HashMap<String, String> getNewVoucherCodeData(){
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("voucherCode", "");
+        return result;
+    }
+
+    public static HashMap<String, String> getNewCashOnDeliveryData(){
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("address", "");
+        result.put("deliveryFee", "");
+        return result;
+    }
+
+}
