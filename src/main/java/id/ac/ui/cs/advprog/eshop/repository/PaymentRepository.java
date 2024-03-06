e7ca6c0b00bcf3fbbb2f0cc3e0cc1818405cf185
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-03-06 17:06:57 +0700

    [GREEN] Implement Payment Repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,15 +1,55 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PaymentRepository {
     private List<Payment> payments = new ArrayList<>();
-    public Payment addPayment(Payment payment) {return null;}
-    public Payment getPayment(String id){return null;}
-    public Iterator<Payment> getAllPayments(){return null;}
-    public Payment setStatus(Payment payment, String newStatus){return null;}
+    public Payment addPayment(Payment payment) {
+        return savePayment(payment);
+    }
+
+
+    public Payment getPayment(String id){
+        for (Payment savedPayment : payments){
+            if (savedPayment.getId().equals(id)){
+                return savedPayment;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+    public Iterator<Payment> getAllPayments(){
+        return payments.iterator();
+    }
+
+    public Payment setStatus(Payment payment, String newStatus){
+        if (payment != null && payments.contains(payment)){
+            Payment newPayment = new Payment(payment.getId(), payment.getMethod(),
+                    newStatus, payment.getPaymentData());
+            savePayment(newPayment);
+            return newPayment;
+        }
+        throw new IllegalArgumentException();
+    }
+
+
+    public Payment savePayment(Payment payment){
+        int i = 0;
+        for (Payment savedPayment : payments){
+            if (savedPayment.getId().equals(payment.getId())){
+                payments.remove(i);
+                payments.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        payments.add(payment);
+        return payment;
+    }
 }
